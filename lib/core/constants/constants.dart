7fc1189f9a8fccb789d4798ef532010dab754b8f
giapchicuong <giapchicuong@gmail.com>
2025-05-05 17:31:13 +0700

    build: change api

--- lib/core/constants/constants.dart
+++ lib/core/constants/constants.dart
@@ -1,8 +1,7 @@
-// import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class ApiUrls {
-  // static String get baseURL => dotenv.env['BASE_URL'] ?? '';
-  static String get baseURL => 'https://dummyjson.com';
+  static String get baseURL => dotenv.env['BASE_URL'] ?? '';
   static String get products => '$baseURL/products';
   static String get searchProducts => '$products/search';
 }
